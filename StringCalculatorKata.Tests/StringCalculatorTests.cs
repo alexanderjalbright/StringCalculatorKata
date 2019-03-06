85c2c53fd1c0968bdc2ebb349d222359733ef025
Alex Albright <alexanderjalbright@gmail.com>
2019-03-06 10:40:17 -0500

    Added theory test, and ability to turn one digit string to int

--- StringCalculatorKata.Tests/StringCalculatorTests.cs
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -5,14 +5,37 @@ namespace StringCalculatorKata.Tests
 {
     public class StringCalculatorTests
     {
-        [Fact]
-        public void Test1()
-        {
-            var underTest = new StringCalculator();
+        StringCalculator underTest;
 
+        public StringCalculatorTests()
+        {
+            underTest = new StringCalculator();
+        }
+
+        [Fact]
+        public void Add_Returns_0_For_Empty_String()
+        {
             var result = underTest.Add("");
 
             Assert.Equal(0, result);
+        }
+
+        [Theory()]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        [InlineData("3", 3)]
+        [InlineData("4", 4)]
+        [InlineData("5", 5)]
+        [InlineData("6", 6)]
+        [InlineData("7", 7)]
+        [InlineData("8", 8)]
+        [InlineData("9", 9)]
+        [InlineData("0", 0)]
+        public void Add_Returns_Expected_Output(string input, int expected)
+        {
+            var result = underTest.Add(input);
+
+            Assert.Equal(expected, result);
         }
     }
 }
